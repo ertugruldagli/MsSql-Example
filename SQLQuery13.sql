6e2c966f9f00589c26773646847cee98c5ee640c
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-10 10:10:41 +0300

    c1

--- SQLQuery13.sql
+++ SQLQuery13.sql
@@ -1,14 +1,29 @@
 
-DECLARE @CompanyName varchar(30), @Address nvarchar(20), @City nvarchar(10), @Country nvarchar(20)
+--DECLARE @CompanyName varchar(30), @Address nvarchar(20), @City nvarchar(10), @Country nvarchar(20)
 
-SELECT CompanyName, Address, City, Country
-from Suppliers
-where SupplierID=8
+--SET @CompanyName='Exotica Liquids'
 
-SELECT @CompanyName=CompanyName, @Address= Address, @City=City, @Country=Country
+----SELECT CompanyName, Address, City, Country
+----from Suppliers
+----where SupplierID=8
+
+--SELECT  @Address= Address, @City=City, @Country=Country
+--From Suppliers
+--Where CompanyName =@CompanyName
+
+--Print  CONCAT (@CompanyName + ' þirketinin açýk adresi ' + @Address + ' ' + @City + ' ' + @Country + ' dir')
+----Print  @CompanyName + ' þirketinin açýk adresi ' + @Address + ' ' + @City + ' ' + @Country + ' dir'
+
+
+---Diðer  yol-------
+
+DECLARE @CompanyName varchar(30), @LongAdress nvarchar(100)
+
+SET @CompanyName='Exotica Liquids'
+
+SELECT  @LongAdress = Address + ' ' + City + ' ' + Country 
 From Suppliers
-Where SupplierID=8
+Where CompanyName = @CompanyName
 
-Print  CONCAT (@CompanyName + ' þirketinin açýk adresi ' + @Address + ' ' + @City + ' ' + @Country + ' dir')
---Print  @CompanyName + ' þirketinin açýk adresi ' + @Address + ' ' + @City + ' ' + @Country + ' dir'
+Print  @CompanyName + ' þirketinin açýk adresi ' + @LongAdress + ' dir'
 
