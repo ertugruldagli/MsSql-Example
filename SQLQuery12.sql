3ed64c936b475f0f18d3aca5ffa656d0908cdb3b
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-10 09:35:25 +0300

    same changces

--- SQLQuery12.sql
+++ SQLQuery12.sql
@@ -1,11 +1,50 @@
-DECLARE @sayi1 int
-DECLARE @ad varchar (15)
-DECLARE @soyad varchar(25)
+--DECLARE @sayi1 int
+--DECLARE @ad varchar (15)
+--DECLARE @soyad varchar(25)
 
-SET @sayi1=5
+--SET @sayi1=5
 
-print 'Girdiðiniz sayi: ' + Convert (nvarchar(20), @sayi1)
+--print 'Girdiðiniz sayi: ' + Convert (nvarchar(20), @sayi1)
 
-SET @ad='Ahmet'
-SET @soyad ='Mehmet'
-SELECT @ad as Ad, @soyad as Soyad
+--SET @ad='Ahmet'
+--SET @soyad ='Mehmet'
+--SELECT @ad as Ad, @soyad as Soyad
+
+--print 'Girdiðiniz Ad Soyad: ' + Concat (@ad, ' ' , @soyad)
+
+----------------------------------------------------------------------------
+
+--DECLARE @FirstName nvarchar(20), @LastName nvarchar(20)
+
+--SELECT FirstName, LastName
+--from Employees
+--where EmployeeID=5
+
+--SELECT @FirstName=FirstName, @LastName=LastName
+--FROM Employees
+--WHERE EmployeeID=5
+
+--Print 'FirstName Degeri';
+--Print @FirstName;
+--Print 'LastName Degeri';
+--Print @LastName;
+
+---------------------------------------------------------------------
+--DECLARE @FirstName nvarchar(20), @LastName nvarchar(20), @Title nvarchar(25);
+
+--SELECT FirstName, LastName, Title
+--from Employees
+--where EmployeeID=5
+
+--SELECT @FirstName=FirstName, @LastName=LastName, @Title=Title
+--FROM Employees
+--WHERE EmployeeID=5
+
+--Print 'FirstName Degeri';
+--Print @FirstName;
+--Print 'LastName Degeri';
+--Print @LastName;
+--Print 'Title Degeri';
+--Print @Title;
+
+-------------------------------------------------------------------------
