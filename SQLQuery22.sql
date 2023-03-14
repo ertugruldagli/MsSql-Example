f438c81f9fa8b7da3d3288e99dab33c5ea15094b
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-14 09:45:48 +0300

    same changes

--- SQLQuery22.sql
+++ SQLQuery22.sql
@@ -1,1 +1,8 @@
 --ürün ortalama fiyatýnýn altýnda fiyata sahip ürünlerin adý ve fiyatýný listeleyen sorgu
+
+Select ProductName, UnitPrice
+From Products
+where ProductName In (
+	Select AVG(UnitPrice) as 'Ortalama Fiyat'
+	From Products
+	having AVG(UnitPrice)<UnitPrice)
