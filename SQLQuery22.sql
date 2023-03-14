b9fc457f9bde61b30e9da13a3edbcc3373e95224
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-14 09:52:10 +0300

    same chancges

--- SQLQuery22.sql
+++ SQLQuery22.sql
@@ -1,8 +1,19 @@
---ürün ortalama fiyatýnýn altýnda fiyata sahip ürünlerin adý ve fiyatýný listeleyen sorgu
+----ürün ortalama fiyatýnýn altýnda fiyata sahip ürünlerin adý ve fiyatýný listeleyen sorgu
 
-Select ProductName, UnitPrice
-From Products
-where UnitPrice < (
-	Select AVG(UnitPrice) 
-	From Products)
+--Select ProductName, UnitPrice
+--From Products
+--where UnitPrice < (
+--	Select AVG(UnitPrice) 
+--	From Products)
+
+-----------------------------------------------------
+
+--Select* 
+--from Orders
+--where Freight Between 10  and 50
+--order by Freight desc
+
+-----------------------------------------------------
+
+
 	
