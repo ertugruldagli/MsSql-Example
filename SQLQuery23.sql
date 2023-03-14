b70ae6bb41540c56f27a2810fd248da8bfd4e443
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-14 10:22:51 +0300

    same changes

--- SQLQuery23.sql
+++ SQLQuery23.sql
@@ -1,4 +1,15 @@
 --sp örnek 
 -- cutomers tablosundaki sehir bilgilerini sayýsý azalacak  þekilde city, adet þekildie listeleyen bir sp oluþtuþturur
 
-CREATE
+--aþaðýdaki PROC  herhangi bir parametre almamaktadýr. Yani static 
+CREATE PROCEDURE spCityList 
+AS
+BEGIN 
+
+	SELECT city, COUNT(city) as 'adet'
+	From Customers
+	group  by city
+	order by count(city) desc
+
+
+END
---
+++ SQLQuery24.sql
@@ -0,0 +1,3 @@
+--sp yi çalýþtýrma
+
+exec dbo.spCityList
