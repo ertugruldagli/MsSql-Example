18736064987145e5106419aac0dc14b18c4c7139
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-14 14:21:17 +0300

    same changes

--- SQLQuery30.sql
+++ SQLQuery30.sql
@@ -21,4 +21,17 @@ For
 	--þimdi bu cursorun birinci kaydýna 1. kaydýna ulaþmak lazým ki okumaya baþlayalým
 	--ayný anda okuduðum veriyi deðiþkenlerime aktarayým...
 
+	--FETCH komutu okuyor/almaya baþlýyor  ve deðiþkenlerime yerleþtiriyor.
+	FETCH NEXT FROM curEmployees INTO @FirstName, @LastName, @Title
+	--- bir tane sistem deðiþkeni var.. Adý @@Fecth_Status
+	--- Okuma Ýþlemini acaba Doðru yaptým mý kontrolü için kullanacaðým
+	--yani iþlem baþarýlý ise @@FETCH_Status deðeri 0(Sýfýrdýr) ve iþlem baþarýlýdýr yani bir sonraki kayýt da mevcuttur.
+
+	while @@FETCH_STATUS =0 -- Yani kayýt varsa iþlemini yap
+		begin 
+		print Concat(@FirstName,' ',@LastName, ' - ', @Title)
+
+		FETCH NEXT FROM curEmployees INTO
+		@FirstName, @LastName, @Title --bir sonraki kaydý okuma
+		end
 END
