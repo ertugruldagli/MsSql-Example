ef5f1387cece53179657fca976e6e6b0daa4b699
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-14 13:36:07 +0300

    commit the Query29

---
+++ SQLQuery29.sql
@@ -0,0 +1,23 @@
+ --- olayda br bölme iþlemi var . Hata olabilir düþücesiyle bu hesaplama/iþlem kýsmýný bir 
+ ---try catch bloklarý arasýna almak yoluyla buradadki hatalarýn neler oldupunu görebilir.
+
+CREATE PROC spDivide
+(@a decimal, @b decimal, @c decimal output)
+AS
+BEGIN
+
+	BEGIN TRY --
+		SET @C=@A/@B;
+	END TRY
+	
+	BEGIN CATCH 
+		SELECT 
+			ERROR_NUMBER()AS ErrorNumber,
+			ERROR_SEVERITY()AS ErrorSeverity,
+			ERROR_STATE()AS ErrorState,
+			ERROR_PROCEDURE()AS ErrorProcedure,
+			ERROR_LINE()AS ErrorLine,
+			ERROR_MESSAGE()AS ErrorMessage
+	END CATCH
+
+END;
