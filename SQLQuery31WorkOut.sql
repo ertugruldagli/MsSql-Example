2eef850993e6657f39de4ed109053db7a68b064b
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-15 09:46:07 +0300

    commit the workout

---
+++ SQLQuery31WorkOut.sql
@@ -0,0 +1,8 @@
+CREATE PROC spShippersCRUD
+(@ShipperID int, @OpCode char(1), @CompanyName nvarchar(40), @Phone nvarchar(24))---PARAMETRE TANIM ALANI
+AS
+BEGIN 
+
+-- Önclikle tablonun þimdikini durumunu listele
+
+END
