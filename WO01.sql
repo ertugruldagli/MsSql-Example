ff9bc4b9303adf736c0518864c2fb6e80724f648
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-15 19:04:33 +0300

    the commit

--- WO01.sql
+++ WO01.sql
@@ -9,3 +9,15 @@
 --order by Phone asc
 
 -----------------------------------------------
+
+--Select  HireDate, FirstName, LastName
+--From Employees
+--Order by HireDate desc
+
+-------------------------------------------------
+
+--Select top 10 OrderId,OrderDate, ShippedDate, CustomerID,Freight
+--from Orders
+--order by Freight desc
+
+------------------------------------------------
