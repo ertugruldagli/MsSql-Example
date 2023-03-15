4e5da16085e5f30ee57ce9b82707215051d0ee23
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-15 19:24:22 +0300

    the commit

--- WO01.sql
+++ WO01.sql
@@ -1,23 +1,55 @@
+--1.)
 --select CategoryName, Description 
 --from Categories
 --order by CategoryName asc
 
 ----------------------------------------------
-
+--2.)
 --Select CompanyName,ContactName,ContactTitle , Phone
 --from Customers
 --order by Phone asc
 
 -----------------------------------------------
-
---Select  HireDate, FirstName, LastName
+--3.)
+--Select  HireDate, FirstName,  upper (LastName)
 --From Employees
 --Order by HireDate desc
 
 -------------------------------------------------
-
+--4.)
 --Select top 10 OrderId,OrderDate, ShippedDate, CustomerID,Freight
 --from Orders
 --order by Freight desc
+
+------------------------------------------------
+--5.)
+--Select lower( CustomerID) as 'Kimlik Tnm'
+--From Customers
+
+------------------------------------------------
+--6.)
+--Select CompanyName, Fax, Phone, Country
+--From Suppliers
+--where(order by CompanyName asc)
+--order by Country desc 
+
+------------------------------------------------
+--7.)
+--Select CompanyName, ContactName
+--From Customers
+--where city='Buenos Aires'
+
+--------------------------------------------------
+
+------------------------------------------------
+
+------------------------------------------------
+
+
+
+------------------------------------------------
+
+--Select lower( CustomerID) as 'Kimlik Tnm'
+--From Customers
 
 ------------------------------------------------
