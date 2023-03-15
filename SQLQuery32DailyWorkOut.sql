3c3f341b54aa42cd46f17c9f6e030051fc29c471
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-15 11:56:59 +0300

    c1

--- SQLQuery32DailyWorkOut.sql
+++ SQLQuery32DailyWorkOut.sql
@@ -1,1 +1,14 @@
-CREATE PROC sp
+alter PROC spSiparisZam
+AS
+BEGIN 
+	--declare curFreight CURSOR
+	 
+			select p.ProductID, od.OrderID,o.Freight as 'yeni fiyat',od.UnitPrice
+			from Orders o
+			inner join [Order Details] od on o.OrderID=od.OrderID
+			inner Join Products p on p.ProductID = od.ProductID
+			where p.ProductID between 10 and 50	 (Select Freight*1.10 from Orders )
+
+END
+
+
