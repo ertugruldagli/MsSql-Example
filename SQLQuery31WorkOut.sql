0b088b3b6e53224eb1ee6334bdf8513ca5baea61
Ertugrul Dagli <ertugruldagl@gmail.com>
2023-03-15 10:03:55 +0300

    c1

--- SQLQuery31WorkOut.sql
+++ SQLQuery31WorkOut.sql
@@ -4,5 +4,27 @@ AS
 BEGIN 
 
 -- Önclikle tablonun þimdikini durumunu listele
+	select *
+	from Shippers
+
+--iþlemler bölümü
+	IF @ShipperID <> 0
+		begin --bir kayýt için tabloda bulunan bir kayýt için R,U,D iþlerinlerinden biri isteniyormuþ
+			if @OpCode = 'R' --Read
+				Select * from Shippers where ShipperID=@ShipperID
+
+			if @OpCode = 'U' --Update
+				Update Shippers set CompanyName=@CompanyName, Phone=@Phone where ShipperID=@ShipperID
+
+			if @OpCode = 'D' --DELETE
+				DELETE from Shippers where ShipperID=@ShipperID
+		end
+
+		else --- ShippersID=0 ise demekki yeni kayýt yapmam isteniyormuþ
+			begin 
+				INSERT INTO Shippers (CompanyName,Phone) VALUES (@CompanyName,@Phone)
+			end
+
+Select* from Shippers
 
 END
